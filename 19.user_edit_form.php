04932ba34845a2112474dbf3bdaf3b5dd960602b
Demi0530 <demi950530123@gmail.com>
2026-06-14 13:16:44 +0800

    本程式用來查詢指定使用者資料，並將資料顯示在修改表單中，讓管理者修改密碼後送出更新。

---
+++ 19.user_edit_form.php
@@ -0,0 +1,27 @@
+<html>
+    <head><title>修改使用者</title></head>
+    <body>
+    <?php
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {     // 檢查是否已登入
+        echo "請登入帳號";      // 未登入則顯示訊息
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{   
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+        $result=mysqli_query($conn, "select * from user where id='{$_GET['id']}'");      // 查詢指定使用者資料
+
+        $row=mysqli_fetch_array($result);     // 取得查詢結果
+        echo "
+        <form method=post action=20.user_edit.php>
+            <input type=hidden name=id value={$row['id']}>
+            帳號：{$row['id']}<br> 
+            密碼：<input type=text name=pwd value={$row['pwd']}><p></p>
+            <input type=submit value=修改>
+        </form>
+        ";
+    }
+    ?>
+    </body>
+</html>
