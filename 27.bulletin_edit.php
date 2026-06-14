f58e539c64767a9a1bbfe3a15f817e2e1b61f4d7
Demi0530 <demi950530123@gmail.com>
2026-06-14 13:44:21 +0800

    接收修改後的佈告資料，更新資料庫中的佈告內容，並顯示修改結果後返回佈告欄列表頁面。

---
+++ 27.bulletin_edit.php
@@ -0,0 +1,22 @@
+<?php
+
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{   
+      // 連接資料庫
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");    
+       // 更新指定佈告資料
+        if (!mysqli_query($conn, "update bulletin set title='{$_POST['title']}',content='{$_POST['content']}',time='{$_POST['time']}',type={$_POST['type']} where bid='{$_POST['bid']}'")){
+            echo "修改錯誤";         
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+        }else{
+            echo "修改成功，三秒鐘後回到佈告欄列表";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+        }
+    }
+
+?>
