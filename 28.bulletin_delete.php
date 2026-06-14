b30e31df3403e7b84eeea83144733f3b364d6258
Demi0530 <demi950530123@gmail.com>
2026-06-14 13:47:10 +0800

    刪除指定的佈告資料，刪除完成後返回佈告欄列表頁面。

---
+++ 28.bulletin_delete.php
@@ -0,0 +1,21 @@
+<?php
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{   
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+        $sql="delete from bulletin where bid='{$_GET["bid"]}'";   // 建立刪除佈告的 SQL 指令
+        // 顯示 SQL（除錯用）
+        // echo $sql;
+        #echo $sql;
+        if (!mysqli_query($conn,$sql)){
+            echo "佈告刪除錯誤";
+        }else{
+            echo "佈告刪除成功";
+        }
+        echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+    }
+?>
